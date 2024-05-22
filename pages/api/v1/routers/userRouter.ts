033e9edb85cc430d49fb91452daf01fbc93a16f1
bisratawoke <bisratawoke52@gmail.com>
2024-05-22 16:51:12 +0300

    Subject Line: inital commit

--- pages/api/v1/routers/userRouter.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { createRouter } from "next-connect";
-
-const router = createRouter<NextApiRequest, NextApiResponse>();
-
-// router.get()
-export default router;
--- pages/services/userService.ts
+++
@@ -1,8 +0,0 @@
-export default class UserService {
-  public static async getUser() {
-    try {
-    } catch (err) {
-      throw new Error("Something went wrong");
-    }
-  }
-}
